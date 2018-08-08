f520f42ea3a25b0ae5ecb7db3e90a6250bf3e69c
jbooth <jason_booth@intuit.com>
2018-08-07 20:36:51 -0700

    Added list control for custom component

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Card extends React.Component {
+    render() {
+        return <div>ABC</div>;
+    }
+}
+
+export default Card
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { SpringGrid } from 'react-stonecutter';
+import Presets from './Presets'
+import Card from './Card'
 
-const Feed = () => (
-    <div className='feed'>
-        <SpringGrid
-            component="ul"
-            columns={1}
-            // columnWidth={150}
-            // gutterWidth={5}
-            gutterHeight={5}
-            itemHeight={200}
-            springConfig={{ stiffness: 170, damping: 26 }}>
-            <li key="A">A</li>
-            <li key="B">B</li>
-            <li key="C">C</li>
-        </SpringGrid>
-    </div>
-)
+class Feed extends React.Component {
+    render() {
+        return
+        <div className='feed'>
+            <SpringGrid
+                component="ul"
+                columns={1}
+                columnWidth={150}
+                gutterWidth={5}
+                gutterHeight={5}
+                itemHeight={200}
+                springConfig={Presets.wobbly}
+            >
+                <li key={'A'}><Card/></li>
+            </SpringGrid>
+        </div>
+    }
+}
 
 export default Feed
