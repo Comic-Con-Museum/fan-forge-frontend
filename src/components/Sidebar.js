826a9b865e4808e1d6613f71a87ae2805da6cb67
Jason Booth <booth.j@husky.neu.edu>
2018-08-08 12:07:41 -0700

    Feed (#11)
    * Added routing for feed
    
    * Added list control for custom component
    
    * Basic grid layout working
    
    * Resolved lint/merge issues

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,9 +12,15 @@ const Sidebar = ({ onClick, path }) => (
     <NavLink activeClassName='active' to='/entry'>
       Idea Page
     </NavLink>
+    <NavLink activeClassName='active' to='/feed'>
+      Feed
+    </NavLink>
   </div>
 )
 
 const mapDispatch = { onClick: goToPage }
 const mapState = ({ location }) => ({ path: location.pathname })
-export default connect(mapState, mapDispatch)(Sidebar)
+export default connect(
+  mapState,
+  mapDispatch
+)(Sidebar)
