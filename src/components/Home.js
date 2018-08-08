abe4a4f7d874546e8254f5048c427416f0924c6d
Owen Sullivan <multiojuice@gmail.com>
2018-08-08 11:00:02 -0700

    Base of the home page, need designs and need pages to link to

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,22 +4,25 @@ import '../css/Home.css'
 import CCMLogo from '../assets/Logo.png'
 import CCMSketch from '../assets/ccm_sketch.jpeg'
 import CCMBuilding from '../assets/ccm_building.jpeg'
+import CCMSanDiego from '../assets/ccm_sanDiego.jpeg'
 
 
 const items = [
   {
     src: CCMSketch,
-    altText: 'Slide 1',
-    caption: 'Slide 1'
-  },
-  {
-    src: CCMLogo,
-    altText: 'Slide 2',
-    caption: 'Slide 2'
+    altText: 'Submit a Design',
+    header: 'Submit A design',
+    caption: 'Fan curated designs can make their way into the museum with enough support!'
   },
   {
     src: CCMBuilding,
-    altText: 'Slide 3',
+    altText: 'About Comic-Con Museum',
+    header: 'About Comic-Con Museum',
+    caption: 'Learn more about Comic-Con\'s year round experience'
+  },
+  {
+    src: CCMSanDiego,
+    header: 'Slide 3',
     caption: 'Slide 3'
   }
 ]
@@ -27,8 +30,8 @@ const items = [
 const Home = () => (
   <div className='home'>
     <div className='fullTop'>
-      <img className='top-left iconimg' src={CCMLogo} alt='Logo' />
       <UncontrolledCarousel className='fullimg' items={items} />
+      <img className='top-left iconimg' src={CCMLogo} alt='Logo' />
       <h1> You <a href='http://www.yahoo.com'>create</a>. We <a href='http://www.yahoo.com'>Display</a></h1>
     </div>
     <div>
