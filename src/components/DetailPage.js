505ae0630387487d4aeb4525d419505267fa0407
Yujia Lu <yujia_lu@intuit.com>
2018-08-08 13:14:43 -0700

    fix style

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {
-  Button, 
+  Button,
   Container,
   Row,
   Col,
@@ -32,10 +32,9 @@ const images = [
 ]
 
 class DetailPage extends Component {
-
   constructor() {
     super()
-    this.state = { activeIndex: 0, activeTab: '1'}
+    this.state = { activeIndex: 0, activeTab: '1' }
   }
 
   onExiting = () => {
@@ -48,13 +47,13 @@ class DetailPage extends Component {
 
   next = () => {
     if (this.animating) return
-    const nextIndex = this.state.activeIndex === images.length - 1 ? 0 : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
+    const nextIndex = this.state.activeIndex === images.length - 1 ? 0 : this.state.activeIndex + 1
+    this.setState({ activeIndex: nextIndex })
   }
 
   previous = () => {
     if (this.animating) return
-    const nextIndex = this.state.activeIndex === 0 ? images.length - 1 : this.state.activeIndex - 1;
+    const nextIndex = this.state.activeIndex === 0 ? images.length - 1 : this.state.activeIndex - 1
     this.setState({ activeIndex: nextIndex })
   }
 
@@ -71,26 +70,24 @@ class DetailPage extends Component {
     }
   }
 
-  render () {
-    const { activeIndex } = this.state;
-    const slides = images.map((img) => {
-      return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={img.src}
-        >
-          <img src={img.src} alt={img.altText} />
-          <CarouselCaption captionText={img.caption} captionHeader={img.caption} />
-        </CarouselItem>
-      );
-    });
+  render() {
+    const { activeIndex } = this.state
+    const slides = images.map(img => (
+      <CarouselItem
+        onExiting={this.onExiting}
+        onExited={this.onExited}
+        key={img.src}
+      >
+        <img src={img.src} alt={img.altText} />
+        <CarouselCaption captionText={img.caption} captionHeader={img.caption} />
+      </CarouselItem>
+    ))
 
     return (
-      <div> 
+      <div>
         <Container fluid>
           <Row>
-            <Col sm="9"> 
+            <Col sm='9'>
               <Carousel
                 activeIndex={activeIndex}
                 next={this.next}
@@ -98,17 +95,17 @@ class DetailPage extends Component {
               >
                 <CarouselIndicators items={images} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
                 {slides}
-                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                <CarouselControl direction='prev' directionText='Previous' onClickHandler={this.previous} />
+                <CarouselControl direction='next' directionText='Next' onClickHandler={this.next} />
               </Carousel>
             </Col>
-            <Col sm="3"> 
-              <div className="buttonWrapper">
+            <Col sm='3'>
+              <div className='buttonWrapper'>
                 <Button>Like This Idea!</Button>
               </div>
               <h4>325 people liked this idea!</h4>
               <Card>
-                <CardImg top width="100%" src="https://www.thefamouspeople.com/profiles/images/stan-lee-5.jpg" />
+                <CardImg top width='100%' src='https://www.thefamouspeople.com/profiles/images/stan-lee-5.jpg' />
                 <CardBody>
                   <CardTitle>Stan Lee</CardTitle>
                   <CardSubtitle>comic-book writer</CardSubtitle>
@@ -118,8 +115,9 @@ class DetailPage extends Component {
               </Card>
             </Col>
           </Row>
-          <div className="detail-body">
-            <SingleLineGridList className="content-list" />
+          <div className='detail-body'>
+            <h4>Submitted Artifacts</h4>
+            <SingleLineGridList />
             <h4>Description</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.</p>
             <h4>Inspiration</h4>
@@ -128,16 +126,14 @@ class DetailPage extends Component {
               <NavItem>
                 <NavLink
                   className={classnames({ active: this.state.activeTab === '1' })}
-                  onClick={() => { this.toggle('1'); }}
-                >
+                  onClick={() => {this.toggle('1')}}>
                   Comments
                 </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink
                   className={classnames({ active: this.state.activeTab === '2' })}
-                  onClick={() => { this.toggle('2'); }}
-                >
+                  onClick={() => {this.toggle('2')}}>
                   Updates
                 </NavLink>
               </NavItem>
@@ -145,7 +141,7 @@ class DetailPage extends Component {
           </div>
 
         </Container>
-    </div>)
+      </div>)
   }
 }
 
--- src/components/SingleLineGridList.js
+++ src/components/SingleLineGridList.js
@@ -1,111 +1,78 @@
-import React from 'react';
-import {
-    withStyles
-} from '@material-ui/core/styles';
-import {
-    GridList,
-    GridListTile,
-    GridListTileBar,
-    IconButton
-} from '@material-ui/core/'
-import StarBorderIcon from '@material-ui/icons/StarBorder';
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { GridList, GridListTile, GridListTileBar, IconButton } from '@material-ui/core/'
+import StarBorderIcon from '@material-ui/icons/StarBorder'
 
-const tileData = [{
-        img: 'https://img.etsystatic.com/il/7ae4a6/714259788/il_fullxfull.714259788_qa6m.jpg?version=0',
-        title: 'Image',
-        author: 'author'
-    },
-    {
-        img: 'https://img.etsystatic.com/il/7ae4a6/714259788/il_fullxfull.714259788_qa6m.jpg?version=0',
-        title: 'Image',
-        author: 'author'
-    },
-    {
-        img: 'https://img.etsystatic.com/il/7ae4a6/714259788/il_fullxfull.714259788_qa6m.jpg?version=0',
-        title: 'Image',
-        author: 'author'
-    },
-    {
-        img: 'https://img.etsystatic.com/il/7ae4a6/714259788/il_fullxfull.714259788_qa6m.jpg?version=0',
-        title: 'Image',
-        author: 'author'
-    }
-];
+const tileData = [
+  {
+    img: 'https://img.etsystatic.com/il/7ae4a6/714259788/il_fullxfull.714259788_qa6m.jpg?version=0',
+    title: 'Image',
+    author: 'author'
+  },
+  {
+    img: 'https://i0.wp.com/www.partyowlmanila.com/wp-content/uploads/2017/09/batmankids.jpg?fit=540%2C540&w=640',
+    title: 'Image',
+    author: 'author'
+  },
+  {
+    img: 'https://c1.staticflickr.com/8/7333/27122863934_c86f2af42c_b.jpg',
+    title: 'Image',
+    author: 'author'
+  },
+  {
+    img: 'http://www.blastoffcomics.com/wp-content/uploads/2017/09/Untitled-design156.png',
+    title: 'Image',
+    author: 'author'
+  }
+]
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-        flexWrap: 'nowrap',
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-        transform: 'translateZ(0)',
-    },
-    title: {
-        color: theme.palette.primary.light,
-    },
-    titleBar: {
-        background: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
-});
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)'
+  },
+  title: {
+    color: theme.palette.primary.light
+  },
+  titleBar: {
+    background: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  }
+})
 
 function SingleLineGridList(props) {
-    const {
-        classes
-    } = props;
+  const { classes } = props
 
-    return ( <
-        div className = {
-            classes.root
-        } >
-        <
-        GridList className = {
-            classes.gridList
-        }
-        cols = {
-            2.5
-        } > {
-            tileData.map(tile => ( <
-                GridListTile key = {
-                    tile.img
-                } >
-                <
-                img src = {
-                    tile.img
-                }
-                alt = {
-                    tile.title
-                }
-                /> <
-                GridListTileBar title = {
-                    tile.title
-                }
-                classes = {
-                    {
-                        root: classes.titleBar,
-                        title: classes.title,
-                    }
-                }
-                actionIcon = { <
-                    IconButton >
-                    <
-                    StarBorderIcon className = {
-                        classes.title
-                    }
-                    /> < /
-                    IconButton >
-                }
-                /> < /
-                GridListTile >
-            ))
-        } <
-        /GridList> < /
-        div >
-    );
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  )
 }
 
-export default withStyles(styles)(SingleLineGridList);
+export default withStyles(styles)(SingleLineGridList)
