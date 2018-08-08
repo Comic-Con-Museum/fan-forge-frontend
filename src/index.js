31947edc6800f32628868b2251cc1758cc61a2fb
Cristi Dragomir <cristi_dragomir@intuit.com>
2018-08-08 13:10:48 -0700

    modify index

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = configureStore(history)
 const render = App =>
   ReactDOM.render(
     <Provider store={store}>
-      <App locale='en'/>
+      <App />
     </Provider>,
     document.getElementById('root')
   )
