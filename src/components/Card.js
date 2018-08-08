f5a70076ad134518c59d35c1c147a76cb0e961b4
jbooth <jason_booth@intuit.com>
2018-08-08 11:16:23 -0700

    Basic grid layout working

--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class Card extends React.Component {
-    render() {
-        return <div>ABC</div>;
-    }
-}
-
-export default Card
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import { SpringGrid } from 'react-stonecutter';
 import Presets from './Presets'
-import Card from './Card'
+import ExhibitCard from './ExhibitCard'
+import cardData from '../mockdata/cards.json'
+
+// springConfig={Presets.wobbly}
 
 class Feed extends React.Component {
     render() {
-        return
-        <div className='feed'>
-            <SpringGrid
-                component="ul"
-                columns={1}
-                columnWidth={150}
-                gutterWidth={5}
-                gutterHeight={5}
-                itemHeight={200}
-                springConfig={Presets.wobbly}
-            >
-                <li key={'A'}><Card/></li>
-            </SpringGrid>
-        </div>
+        return <div className='feed'>
+                <SpringGrid
+                    component="ul"
+                    columns={1}
+                    columnWidth={700}
+                    gutterWidth={10}
+                    gutterHeight={10}
+                    itemHeight={400}
+                >
+                    {cardData.map(card =>
+                        <li key={card.title}>
+                            <ExhibitCard title={card.title} picture={card.picture} summary={card.summary}/>
+                        </li>)}
+                </SpringGrid>
+            </div>
     }
 }
 
