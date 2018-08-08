694ebc7daa366d6cb016570ad96faa4acfa827e1
Yujia Lu <yujia_lu@intuit.com>
2018-08-08 11:19:21 -0700

    add content grid list

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Button,
+import {
+  Button, 
   Container,
   Row,
   Col,
@@ -17,8 +18,9 @@ import { Button,
   CardText,
   CardSubtitle,
   CardTitle } from 'reactstrap'
+import SingleLineGridList from './SingleLineGridList'
 import '../css/DetailPage.css'
-import classnames from 'classnames';
+import classnames from 'classnames'
 
 
 const images = [
@@ -27,45 +29,45 @@ const images = [
     caption: 'Title',
     altText: 'Batman'
   }
-];
+]
 
 class DetailPage extends Component {
 
   constructor() {
-    super();
-    this.state = { activeIndex: 0, activeTab: '1'};
+    super()
+    this.state = { activeIndex: 0, activeTab: '1'}
   }
 
   onExiting = () => {
-    this.animating = true;
+    this.animating = true
   }
 
   onExited = () => {
-    this.animating = false;
+    this.animating = false
   }
 
   next = () => {
-    if (this.animating) return;
+    if (this.animating) return
     const nextIndex = this.state.activeIndex === images.length - 1 ? 0 : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
   }
 
   previous = () => {
-    if (this.animating) return;
+    if (this.animating) return
     const nextIndex = this.state.activeIndex === 0 ? images.length - 1 : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
+    this.setState({ activeIndex: nextIndex })
   }
 
-  goToIndex = (newIndex) => {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
+  goToIndex = newIndex => {
+    if (this.animating) return
+    this.setState({ activeIndex: newIndex })
   }
 
-  toggle(tab) {
+  toggle = tab => {
     if (this.state.activeTab !== tab) {
       this.setState({
         activeTab: tab
-      });
+      })
     }
   }
 
@@ -88,7 +90,7 @@ class DetailPage extends Component {
       <div> 
         <Container fluid>
           <Row>
-            <Col xs="9"> 
+            <Col sm="9"> 
               <Carousel
                 activeIndex={activeIndex}
                 next={this.next}
@@ -100,11 +102,12 @@ class DetailPage extends Component {
                 <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
               </Carousel>
             </Col>
-            <Col xs="3"> 
+            <Col sm="3"> 
               <div className="buttonWrapper">
-                <Button>Support This Idea!</Button>
+                <Button>Like This Idea!</Button>
               </div>
-              <Card style={{ backgroundColor: '#333', borderColor: '#333' }}>
+              <h4>325 people liked this idea!</h4>
+              <Card>
                 <CardImg top width="100%" src="https://www.thefamouspeople.com/profiles/images/stan-lee-5.jpg" />
                 <CardBody>
                   <CardTitle>Stan Lee</CardTitle>
@@ -115,25 +118,31 @@ class DetailPage extends Component {
               </Card>
             </Col>
           </Row>
-
-          <Nav tabs>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === '1' })}
-                onClick={() => { this.toggle('1'); }}
-              >
-                Comments
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === '2' })}
-                onClick={() => { this.toggle('2'); }}
-              >
-                Updates
-              </NavLink>
-            </NavItem>
-          </Nav>
+          <div className="detail-body">
+            <SingleLineGridList className="content-list" />
+            <h4>Description</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.</p>
+            <h4>Inspiration</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.</p>
+            <Nav tabs>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: this.state.activeTab === '1' })}
+                  onClick={() => { this.toggle('1'); }}
+                >
+                  Comments
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: this.state.activeTab === '2' })}
+                  onClick={() => { this.toggle('2'); }}
+                >
+                  Updates
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </div>
 
         </Container>
     </div>)
