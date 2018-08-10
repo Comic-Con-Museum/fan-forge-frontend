0597827afa30cd3706c8bfb26a94f999a71ab1a2
Yujia Lu <yujia_lu@intuit.com>
2018-08-09 17:15:49 -0700

    add comment list on detail page and improve ui

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -14,13 +14,19 @@ import {
   CardText,
   CardSubtitle,
   CardTitle,
-  CardLink } from 'reactstrap'
+  CardLink,
+  Row,
+  Col,
+  TabContent,
+  TabPane } from 'reactstrap'
 import SingleLineGridList from './SingleLineGridList'
 import { Grid, Button, Avatar } from '@material-ui/core'
 import '../css/DetailPage.css'
 import classnames from 'classnames'
 import { withStyles } from '@material-ui/core/styles'
 import Collapsible from 'react-collapsible'
+import CommentsList from './CommentsList'
+
 
 
 const images = [
@@ -54,7 +60,7 @@ class DetailPage extends Component {
     this.state = { activeIndex: 0, 
       activeTab: '1',
       description: true,
-      inspiration: true }
+      inspiration: false }
   }
 
   onExiting = () => {
@@ -112,8 +118,7 @@ class DetailPage extends Component {
                 activeIndex={activeIndex}
                 next={this.next}
                 previous={this.previous}
-                pause='hover'
-              >
+                pause='hover'>
                 <CarouselIndicators items={images} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
                 {slides}
                 <CarouselControl direction='prev' directionText='Previous' onClickHandler={this.previous} />
@@ -121,11 +126,6 @@ class DetailPage extends Component {
               </Carousel>
             </Grid>
             <Grid item sm={3}>
-              <div className='buttonWrapper'>
-                <Button variant="contained">Like This Idea!
-                </Button>
-              </div>
-              <h4>325 people liked this idea!</h4>
               <Card>
                 <Avatar className={classnames(classes.avatar, classes.bigAvatar)} src='https://www.thefamouspeople.com/profiles/images/stan-lee-5.jpg'/>
                 <CardBody>
@@ -135,6 +135,10 @@ class DetailPage extends Component {
                   <CardLink href='#'>Follow</CardLink>
                 </CardBody>
               </Card>
+              <div className='buttonWrapper'>
+                <h4>325 people liked this idea!</h4>
+                <Button variant="contained">Like This Idea!</Button>
+              </div>
             </Grid>
           </Grid>
           <div className='detail-body'>
@@ -144,7 +148,7 @@ class DetailPage extends Component {
             <Collapsible trigger="Description" open>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.</p>
             </Collapsible>
-            <Collapsible trigger="Inspiration" open>
+            <Collapsible trigger="Inspiration">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.</p>
             </Collapsible>
             <Nav tabs>
@@ -163,6 +167,22 @@ class DetailPage extends Component {
                 </NavLink>
               </NavItem>
             </Nav>
+            <TabContent activeTab={this.state.activeTab}>
+              <TabPane tabId="1">
+                <Row>
+                  <Col xs="12">
+                    <CommentsList />
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="2">
+                <Row>
+                  <Col xs="12">
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. </h5>
+                  </Col>
+                </Row>
+              </TabPane>
+            </TabContent>
           </div>
       </div>)
   }
