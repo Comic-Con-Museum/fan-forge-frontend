238b49428fafb1cdca774e1b1cf4c754adb9f298
Nihar Patel <NihPat95@gmail.com>
2018-08-09 20:45:00 -0700

    added header for post call

--- src/actions/index.js
+++ src/actions/index.js
@@ -48,12 +48,13 @@ export const url = 'https://yu1pn4u266.execute-api.us-west-2.amazonaws.com/lates
 export const postExhibit = (exhibit) => {
   return (dispatch) => {
     dispatch({type : POST_EXHIBIT});
-
+    console.log(JSON.stringify(exhibit));
     fetch(url, {
       method: 'POST',
       body: JSON.stringify(exhibit),
       headers: {
-        "Content-Type": "application/json;",
+	'Accept': 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
       },
     }).then(res => {
       return res.json();
