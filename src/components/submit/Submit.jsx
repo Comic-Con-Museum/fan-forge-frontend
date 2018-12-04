3ccf696f32697e52dd9ebd211ab5e4b339d52458
Zach Jones <zachtjones16@gmail.com>
2018-12-03 21:07:09 -0800

    Added images row

--- src/components/submit/Submit.jsx
+++ src/components/submit/Submit.jsx
@@ -1,22 +1,97 @@
 import React, { PureComponent } from 'react';
+import AriaModal from 'react-aria-modal';
+import Dropzone from 'react-dropzone'
 
 import {
-  PageWrapper,
-  Title
+  Artifact,
+  ArtifactImg,
+  ImageSection,
+  HorizontalSection,
+  RemoveArtifact,
+  SubmitButton,
+  SubmitForm
 } from './StyledComponents';
 
 class Submit extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        images: [], // base64 data array to show
+        files: [] // references to the file objects
+    };
+  }
+
+  getApplicationNode = () => {
+    return document.getElementById('root');
+  };
+
+  onDrop = (acceptedFiles) => {
+    acceptedFiles.forEach(file => {
+      const reader = new FileReader();
+      reader.onload = () => {
+
+        // append the data url (base64) so that I can do src="..."
+        const fileAsBase64String = reader.result;
+        this.setState(prevState => ({
+          images: [...prevState.images, fileAsBase64String],
+          files: [...prevState.files, file]
+        }));
+      };
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+
+      reader.readAsDataURL(file);
+    });
+  }
+
+  submit = () => {
+    alert("submit clicked");
+
+    this.props.deactivateModal();
+  }
+
+  renderImage = (data, index) => {
+    return (
+      <Artifact
+        key={index}
+      >
+        <RemoveArtifact>
+          X
+        </RemoveArtifact>
+        <ArtifactImg src={data} />
+      </Artifact>
+    );
   }
 
   render() {
     return (
-      <PageWrapper>
-        <Title>Hey There</Title>
-        <Title>This is a sample page: SUBMIT</Title>
-      </PageWrapper>
+      <AriaModal
+        titleText={`Supporting exhibit: ${this.props.title}`}
+        onExit={this.props.deactivateModal}
+        initialFocus="#demo-one-modal"
+        getApplicationNode={this.getApplicationNode}
+        underlayStyle={{ paddingTop: '2em' }}
+      >
+        <SubmitForm id="demo-one-modal">
+          <div>
+            Add some images to show off your exhibit,
+            the first one will be the cover
+          </div>
+          <HorizontalSection>
+            <ImageSection>
+              {this.state.images.map((data, index) => this.renderImage(data, index))}
+            </ImageSection>
+            <Dropzone
+              accept={'image/*'}
+              onDrop={this.onDrop}
+            >
+              Drag and drop some files here, or click to add them
+            </Dropzone>
+          </HorizontalSection>
+
+          <SubmitButton onClick={this.submit}>Submit</SubmitButton>
+        </SubmitForm>
+      </AriaModal>
     );
   }
 }
